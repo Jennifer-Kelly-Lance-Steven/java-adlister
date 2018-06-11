cba8b85e2cb4d96cfb0fafec7e2e157af734f0ad
Jennifer Walker <dataqueen16@gmail.com>
2018-06-11 14:23:46 -0500

    filled out the migration file to create the tables

--- migration.sql
+++ migration.sql
@@ -5,10 +5,11 @@ DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS ads_cat;
 DROP TABLE IF EXISTS categories;
 
+
 CREATE TABLE users (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    username VARCHAR(240) NOT NULL,
-    email VARCHAR(240) NOT NULL,
+    username VARCHAR(240) NOT NULL UNIQUE,
+    email VARCHAR(240) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
 );
@@ -16,27 +17,52 @@ CREATE TABLE users (
 CREATE TABLE ads (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     user_id INT UNSIGNED NOT NULL,
+    ads_cat_p_id INT UNSIGNED NOT NULL,
     title VARCHAR(240) NOT NULL,
     description TEXT NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (user_id) REFERENCES users(id)
+        ON DELETE CASCADE,
+    FOREIGN KEY (ads_cat_p_id) REFERENCES ads_cat_p(id)
         ON DELETE CASCADE
 );
 
-CREATE TABLE ads_cat (
+CREATE TABLE ads_cat_p (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    user_id INT UNSIGNED NOT NULL,
-    title VARCHAR(240) NOT NULL,
-    description TEXT NOT NULL,
+    ads_id INT UNSIGNED NOT NULL,
+    cat_id INT UNSIGNED NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    FOREIGN KEY (ads_id) REFERENCES ads(id)
+        ON DELETE CASCADE,
+    FOREIGN KEY (cat_id) REFERENCES categories(id)
         ON DELETE CASCADE
 );
 
 CREATE TABLE categories (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    user_id INT UNSIGNED NOT NULL,
-    title VARCHAR(240) NOT NULL,
-    description TEXT NOT NULL,
-    PRIMARY KEY (id)
+    cat_subcat_p_id INT UNSIGNED NOT NULL,
+    category VARCHAR(50) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (cat_subcat_p_id) REFERENCES ads_cat_p(id)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE cat_subcat_p (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    cat_id INT UNSIGNED NOT NULL,
+    subcat_id INT UNSIGNED NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (subcat_id) REFERENCES subcat(id)
+        ON DELETE CASCADE,
+    FOREIGN KEY (cat_id) REFERENCES categories(id)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE subcat (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    cat_subcat_p_id INT UNSIGNED NOT NULL,
+    subcat VARCHAR(50) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (cat_subcat_p_id) REFERENCES cat_subcat_p(id)
+        ON DELETE CASCADE
 );
