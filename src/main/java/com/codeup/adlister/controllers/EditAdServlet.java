b062782d04795d5934678b0bcafd60ec224b7fb6
steven <steven.hampton415@gmail.com>
2018-06-14 16:21:21 -0500

    Added edit jsp and basic servlet need function still

--- src/main/java/com/codeup/adlister/controllers/EditAdServlet.java
+++ src/main/java/com/codeup/adlister/controllers/EditAdServlet.java
@@ -1,17 +1,27 @@
-
 package com.codeup.adlister.controllers;
 
 import com.codeup.adlister.dao.MySQLAdsDao;
-
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 
-@WebServlet(name = "controllers.EditAdServlet", urlPatterns = "/edit")
+@WebServlet(name = "controllers.EditAdServlet", urlPatterns = "/ads/edit")
 public class EditAdServlet extends HttpServlet{
-    protected void doPost(HttpServletResponse response, HttpServletRequest request){
-        String adID = request.getParameter("edit");
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        if (request.getSession().getAttribute("user") == null) {
+            response.sendRedirect("/login");
+            return;
+        }
+        request.getParameter("id");
+
+        try {
+            response.sendRedirect("/edit.jsp");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
